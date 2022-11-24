c87d2055aea4f29fb040caca9ad543545e4a187c
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-24 17:15:56 +0100

    Craete processRemark function to accept input and give a remark

--- gameMachine.js
+++ gameMachine.js
@@ -13,6 +13,26 @@ function machineStatus() {
     } 
   }
 
-  btn.addEventListener("click", machineActive);
 //create a function machineStopped to stop processing inputs when machine is stopped.
 //create a function processRemark to give remark for each range of inputted score.
+function processRemark(){
+    const choice = parseInt(input.value);
+    if (choice < 0 || choice > 100){
+        remark.textContent = "Please Insert a Valid Score between 0 and 100"
+    }else if(choice < 20 && choice !== (' ')){
+        remark.textContent = "Whoaaa You Can do Better!";
+    }else if (choice < 40 && choice !== (' ')){
+        remark.textContent = "You are Coming up!";
+    }else if (choice < 60 && choice !== (' ')){
+        remark.textContent = "Yey You are Doing Well!"
+    }else if (choice < 80 && choice !== (' ')){
+        remark.textContent = "Wow What a Good Score You Have There;";
+    }else if (choice <= 100 && choice !== (' ')){
+        remark.textContent = "Wowww What a Great Score You Nailed It!";
+    }else {
+        remark.textContent = "Please Insert A Valid Score!";
+    }
+}
+
+viewRemark.addEventListener("click", processRemark);
+btn.addEventListener("click", machineActive);
